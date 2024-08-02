22f734d16dd4819f3cee36f5fb8a1585431fb561
huadong <lesarde@gmail.com>
2024-08-02 18:58:26 +0800

    expose JdbcTable as public class, fix NPE for ModifiableTable

--- core/src/main/java/org/apache/calcite/adapter/jdbc/JdbcTable.java
+++ core/src/main/java/org/apache/calcite/adapter/jdbc/JdbcTable.java
@@ -87,7 +87,7 @@ public class JdbcTable extends AbstractQueryableTable
   public final String jdbcTableName;
   public final Schema.TableType jdbcTableType;
 
-  JdbcTable(JdbcSchema jdbcSchema, String jdbcCatalogName,
+  public JdbcTable(JdbcSchema jdbcSchema, String jdbcCatalogName,
       String jdbcSchemaName, String jdbcTableName,
       Schema.TableType jdbcTableType) {
     super(Object[].class);
@@ -192,7 +192,7 @@ public class JdbcTable extends AbstractQueryableTable
   }
 
   @Override public @Nullable Collection getModifiableCollection() {
-    return null;
+    return new ArrayList();
   }
 
   @Override public TableModify toModificationRel(RelOptCluster cluster,
